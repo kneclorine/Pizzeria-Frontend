d1343667ef7c7ce9dbbde5fd430e45d5408d2430
carloscastillogarcia <krestinho@gmail.com>
2021-09-20 14:35:49 +0200

    library interceptor

--- projects/core-lib/src/public-api.ts
+++ projects/core-lib/src/public-api.ts
@@ -5,3 +5,10 @@
 export * from './lib/core-lib.service';
 export * from './lib/core-lib.component';
 export * from './lib/core-lib.module';
+export * from './lib/interceptor/decoratorservice';
+export * from './lib/interceptor/interceptorauth';
+export * from './lib/interceptor/interceptordenied';
+export * from './lib/interceptor/interceptorunauthorized';
+export * from './lib/services/authorizationobserver';
+export * from './lib/services/authorize';
+
