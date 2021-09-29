189032c95b1d6d1d67ce434cf4bd3667c96b3e0f
Alexander <59999903+kneclorine@users.noreply.github.com>
2021-09-29 19:17:32 +0200

    Deleted unnecessary import from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppComponent } from './app.component';
 import { IngredientSharedModule } from 'projects/ingredient/src/app/app.module';
 import { UserSharedModule } from 'projects/user/src/app/app.module';
 import { CoreLibModule } from 'core-lib';
-import { CapImguploadComponent } from 'projects/core-lib/src/lib/formcontrols/cap-imgupload/cap-imgupload.component';
 
 
 @NgModule({
