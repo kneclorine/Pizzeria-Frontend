f628514ce6cc33c2ada5008863ffd747f64b0aac
Alexander <59999903+kneclorine@users.noreply.github.com>
2021-10-06 09:11:20 +0200

    Added authorization observer to public-api of core-lib

--- projects/core-lib/src/public-api.ts
+++ projects/core-lib/src/public-api.ts
@@ -6,6 +6,7 @@
 export * from './lib/core-lib.module';
 export * from './lib/interceptor/index';
 export * from './lib/services/authorize';
+export * from './lib/services/authorizationobserver'
 export * from './lib/menu/menu.component';
 export * from './lib/interceptor/decoratorservice';
 export * from './lib/config/config';
